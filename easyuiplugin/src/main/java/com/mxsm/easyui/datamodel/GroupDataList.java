8ca93eb23a3b536ec92942749518ea878321e24b
mxsm <ljbmxsm@163.com>
2015-12-06 17:48:50 +0800

    添加可分组的DataList

---
+++ easyuiplugin/src/main/java/com/mxsm/easyui/datamodel/GroupDataList.java
@@ -0,0 +1,22 @@
+package com.mxsm.easyui.datamodel;
+
+public class GroupDataList extends DataList {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3307713969622846493L;
+	
+	private String group;
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	
+	
+	
+}
